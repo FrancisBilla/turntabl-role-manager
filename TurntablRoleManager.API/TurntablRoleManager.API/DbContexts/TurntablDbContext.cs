cdfe9ed275575f4dc92b987f969bcf92891a02c8
erbynn <john.erbynn@gmail.com>
2020-04-28 17:30:16 +0100

    add employee entity

--- TurntablRoleManager.API/TurntablRoleManager.API/DbContexts/TurntablDbContext.cs
+++ TurntablRoleManager.API/TurntablRoleManager.API/DbContexts/TurntablDbContext.cs
@@ -12,39 +12,39 @@ namespace TurntablRoleManager.API.DbContexts
         }
 
         public DbSet<Role> Roles { get; set; }
-       /* public DbSet<Course> Courses { get; set; }*/
+        public DbSet<Employee> Employees { get; set; }
+      
+        /*  protected override void OnModelCreating(ModelBuilder modelBuilder)
+          {
+              // seed the database with dummy data
+              modelBuilder.Entity<Role>().HasData(
+                  new Role()
+                  {
+                      Id = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                      Name = "Developer",
+                      Description = "Devloping and Managing Application",
+                      CreatedAt = new DateTime(1650, 7, 23),
 
-      /*  protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // seed the database with dummy data
-            modelBuilder.Entity<Role>().HasData(
-                new Role()
-                {
-                    Id = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
-                    Name = "Developer",
-                    Description = "Devloping and Managing Application",
-                    CreatedAt = new DateTime(1650, 7, 23),
-                    
-                },
-                new Role()
-                {
-                    Id = Guid.Parse("da2fd609-d754-4feb-8acd-c4f9ff13ba96"),
-                    Name = "Project Manager",
-                    Description = "Maninging project for the company",
-                    CreatedAt = new DateTime(1650, 7, 29),
-                },
-                new Role()
-                {
-                    Id = Guid.Parse("2902b665-1190-4c70-9915-b9c2d7680450"),
-                    Name = "Accountant",
-                    Description = "Maninging accounts for the company",
-                    CreatedAt = new DateTime(1701, 12, 16)
-                }
-                
-                );
+                  },
+                  new Role()
+                  {
+                      Id = Guid.Parse("da2fd609-d754-4feb-8acd-c4f9ff13ba96"),
+                      Name = "Project Manager",
+                      Description = "Maninging project for the company",
+                      CreatedAt = new DateTime(1650, 7, 29),
+                  },
+                  new Role()
+                  {
+                      Id = Guid.Parse("2902b665-1190-4c70-9915-b9c2d7680450"),
+                      Name = "Accountant",
+                      Description = "Maninging accounts for the company",
+                      CreatedAt = new DateTime(1701, 12, 16)
+                  }
 
-         
-            base.OnModelCreating(modelBuilder);
-        }*/
+                  );
+
+
+              base.OnModelCreating(modelBuilder);
+          }*/
     }
 }
