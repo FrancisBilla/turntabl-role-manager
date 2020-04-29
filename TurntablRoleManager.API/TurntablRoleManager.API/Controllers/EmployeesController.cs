c96b2f4eea1f214d617b492c4ab01e2b96081326
erbynn <john.erbynn@gmail.com>
2020-04-29 14:57:33 +0100

    wip: add getemployees

--- TurntablRoleManager.API/TurntablRoleManager.API/Controllers/EmployeesController.cs
+++ TurntablRoleManager.API/TurntablRoleManager.API/Controllers/EmployeesController.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TurntablRoleManager.API.Models;
+using TurntablRoleManager.API.Services;
 
 namespace TurntablRoleManager.API.Controllers
 {
-    public class EmployeesController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
     {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+
+        public EmployeesController(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<EmployeeTo>> Employees()
+        { 
+            var employees = _employeeRepository.GetEmployees();
+
+            if(employees == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<IEnumerable<EmployeeTo>>(employees));
+        }
     }
 }
