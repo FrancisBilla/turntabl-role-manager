58c3f0be7986c429dc4431c5cb5c86fb972a8667
erbynn <john.erbynn@gmail.com>
2020-04-29 14:24:41 +0100

    wip: add emp read and delete repositories

---
+++ TurntablRoleManager.API/TurntablRoleManager.API/Services/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TurntablRoleManager.API.Entities;
+
+namespace TurntablRoleManager.API.Services
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetEmployees();
+        Employee GetEmployeeById(int id);
+        //Role CreateEmployee(Employee employee);
+        //Role UpdateEmployee(Employee employee);
+        //void DeleteEmployee(Guid id);
+    }
+}
